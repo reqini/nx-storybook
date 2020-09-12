3e43b60d0e6d6272a1e1860a46952f3422ceaa72
jirrazabal <joseirrazabal@gmail.com>
2020-09-12 14:30:59 -0300

    config viewport

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,6 +2,27 @@ import { withMuiTheme } from '@harelpls/storybook-addon-materialui'
 
 import theme from '../src/theme/DefaultTheme'
 
+const customViewports = {
+  hd: {
+    name: '720',
+    styles: {
+      width: '1280px',
+      height: '720px'
+    }
+  },
+  fullHd: {
+    name: '1080',
+    styles: {
+      width: '1280px',
+      height: '1080px'
+    }
+  }
+}
+
+export const parameters = {
+  viewport: { viewports: customViewports }
+}
+
 export const decorators = [
   withMuiTheme({
     'Default theme': theme
